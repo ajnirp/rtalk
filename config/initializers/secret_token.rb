153d37cd4c7afcd53bc793347035ef986b1ae4d4
Rohan Prinja <rohan.prinja@gmail.com>
2013-05-20 17:42:04 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtalk::Application.config.secret_token = '8c8014aeee0224af67155f6fdb517c04ad6415a56237261887728da33167833f3b2f0f1ee083324969c10817f7a76c71d8a13b6b5cfc691d123337e6cd8f5ca8'
